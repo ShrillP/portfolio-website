feb3a859b7a3835beacba5a363c61333f694de2d
Shrill Patel <1006shrillpatel@gmail.com>
2020-10-19 00:17:11 -0400

    Another Update

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,7 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('astar/', views.astar, name='astar'),
-    path('mindspace/', views.mindspace, name='mindspace'),
-    path('passwordvault/', views.passvault, name='passvault')
+    path('astar/', views.astar),
+    path('mindspace/', views.mindspace),
+    path('passwordvault/', views.passvault)
 ]
