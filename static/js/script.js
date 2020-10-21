ccdf9a5d9fc6159f918ed1886ecca04ab2cbe075
Shrill Patel <1006shrillpatel@gmail.com>
2020-10-20 22:48:05 -0400

    Update JS logic

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,5 @@
+{% load static %}
+
 console.log('Its working')
 
 let theme = localStorage.getItem('theme')
@@ -21,16 +23,16 @@ for (var i = 0; themeDots.length > i; i++){
 
 function setTheme(mode){
   if(mode == 'light'){
-    document.getElementById('theme-style').href = 'default.css'
+    document.getElementById('theme-style').href = "{% static 'css/default.css' %}"
   }
   if(mode == 'blue'){
-    document.getElementById('theme-style').href = 'blue.css'
+    document.getElementById('theme-style').href = "{% static 'css/blue.css' %}"
   }
   if(mode == 'green'){
-    document.getElementById('theme-style').href = 'green.css'
+    document.getElementById('theme-style').href = "{% static 'css/green.css' %}"
   }
   if(mode == 'purple'){
-    document.getElementById('theme-style').href = 'purple.css'
+    document.getElementById('theme-style').href = "{% static 'css/purple.css' %}"
   }
 
   localStorage.setItem('theme', mode)
